b60d39885e80acff4ba23030e27423c1d0da459e
mckuratova <107032112+mckuratova@users.noreply.github.com>
2023-04-13 23:24:33 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import ttest_ind
 
+chat_id = 906914964  # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x: np.array, y: np.array) -> bool:
+    pv = ttest_ind(x, y, equal_var=False).pvalue
+    res = False
+    if pv < 0.06:
+      res = True
+    return res # Ваш ответ, True или False
